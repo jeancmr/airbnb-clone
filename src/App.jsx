49443db629ab9f5e69a3eaf5148b406521de02ae
jeancmr <jean.madiedo08@gmail.com>
2023-01-25 17:00:46 -0500

    render arrays

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,27 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
-import katie from "./assets/katie-zaferes.png";
+import cardsData from "./data";
+// import katie from "./assets/katie-zaferes.png";
+// import wedding from "./assets/wedding.png";
+// import bike from "./assets/mountain-bike.png";
 
 export default function App() {
+  const cardElements = cardsData.map((card) => (
+    <Card
+      img={card.coverImg}
+      rating={card.stats.rating}
+      reviewCount={card.stats.reviewCount}
+      country={card.location}
+      title={card.title}
+      price={card.price}
+    />
+  ));
   return (
     <nav className="container">
       <Navbar />
       <Hero />
-      <Card
-        img={katie}
-        rating="5.0"
-        reviewCount={6}
-        country="Colombia"
-        title="Life Lesson with Katie Zaferes"
-        price={136}
-      />
+      <section className="section-cards">{cardElements}</section>
     </nav>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,30 +2,29 @@ import React from "react";
 import star from "../assets/star.png";
 // import swimmer from "../assets/katie-zaferes.png";
 // import swimmer from `../assets/${props.img}`;
+// import jelou from "../wedding.png";
 export default function Card(props) {
   console.log(props);
   return (
-    <section className="section-cards">
-      <div className="card">
-        <img
-          src={props.img}
-          alt="photo of swimmer Katie Zaferes"
-          className="card-img"
-        />
-        <div className="card-text-box">
-          <div className="card-rating">
-            <img src={star} alt="star icon" className="star-img" />
-            <span>{props.rating}</span>
-            <span>&#40;{props.reviewCount}&#41;</span>
-            <span>&#8226;</span>
-            <span>{props.country}</span>
-          </div>
-          <p>{props.title}</p>
-          <p>
-            <strong>From ${props.price}</strong> / person
-          </p>
+    <div className="card">
+      <img
+        src={`../${props.img}`}
+        alt="photo of swimmer Katie Zaferes"
+        className="card-img"
+      />
+      <div className="card-text-box">
+        <div className="card-rating">
+          <img src={star} alt="star icon" className="star-img" />
+          <span>{props.rating}</span>
+          <span>&#40;{props.reviewCount}&#41;</span>
+          <span>&#8226;</span>
+          <span>{props.country}</span>
         </div>
+        <p>{props.title}</p>
+        <p>
+          <strong>From ${props.price}</strong> / person
+        </p>
       </div>
-    </section>
+    </div>
   );
 }
